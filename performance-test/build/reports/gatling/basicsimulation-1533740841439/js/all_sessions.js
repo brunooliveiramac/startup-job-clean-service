3093b1cf9da6de86c9d1a47341013887b4dc87e9
Bruno Macedo <brunom@ciandt.com>
2018-08-08 12:12:15 -0300

    Add performance test

---
+++ performance-test/build/reports/gatling/basicsimulation-1533740841439/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FF9D00',
+name: 'All Users',
+data: [
+  [1533740843000,1],[1533740844000,1],[1533740845000,1],[1533740846000,1],[1533740847000,1],[1533740848000,1],[1533740849000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
---
+++ performance-test/src/gatling/scala/jobusecase/BasicSimulation.scala
@@ -0,0 +1,16 @@
+package jobusecase
+
+import io.gatling.http.Predef._
+import io.gatling.core.Predef._
+
+class BasicSimulation extends Simulation {
+
+  val httpConf = http.baseURL("https://api.football-data.org/")
+  val scn = scenario("Basic Simulation")
+    .exec(http("request_1")
+      .get("v1/teams/73"))
+    .pause(5)
+  setUp(
+    scn.inject(atOnceUsers(1))
+  ).protocols(httpConf)
+}
