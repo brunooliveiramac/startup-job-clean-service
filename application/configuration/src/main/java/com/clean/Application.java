90385dc1fc0431914095a1ee45230f64165fe641
Bruno Macedo <brunom@ciandt.com>
2018-08-09 14:46:08 -0300

    fix ff4j thymeleaf version compability

--- application/configuration/src/main/java/com/clean/Application.java
+++ application/configuration/src/main/java/com/clean/Application.java
@@ -2,12 +2,10 @@ package com.clean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPathProvider;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+
 
 @EnableEurekaClient
 @Configuration
--- application/configuration/src/main/java/com/clean/configuration/toggle/DispatcherServletPathProvider.java
+++ application/configuration/src/main/java/com/clean/configuration/toggle/DispatcherServletPathProvider.java
@@ -1,17 +1,19 @@
 package com.clean.configuration.toggle;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-@Component
-public class DispatcherServletPathProvider implements org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPathProvider {
+//@Primary
+//@Component
+public class DispatcherServletPathProvider { //implements org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPathProvider {
 
-    @Value("${server.servlet.context-path}")
-    private String servletContextPath;
-
-    @Override
-    public String getServletPath() {
-        return servletContextPath;
-    }
+//    @Value("${server.servlet.context-path}")
+//    private String servletContextPath;
+//
+//    @Override
+//    public String getServletPath() {
+//        return servletContextPath;
+//    }
 
 }
